d179982876009cf4505fd4b4a5f43aaff434c31a
kuntesh87 <kuntesh87@gmail.com>
2018-05-28 17:47:35 -0700

    Added reference to CSS

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
+import './index.css';
 import registerServiceWorker from './registerServiceWorker';
 import { Provider } from 'react-redux';
 import configureStore from './store/configureStore';
